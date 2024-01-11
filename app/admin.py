26eea40e443f82d019aadbd4fa7392cd121ae905
seaz96 <seaz442@mail.ru>
2024-01-12 00:15:56 +0500

    some fixes

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+from app import views
 from app.models import Vacancy
 
 admin.site.register(Vacancy)
--- app/models.py
+++ app/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Vacancy(models.Model):
-    name = models.TextField
-    key_skills = models.TextField
-    salary_from = models.FloatField
-    salary_to = models.FloatField
-    salary_currency = models.TextField
-    area_name = models.TextField
-    published_at = models.TextField
+    name = models.TextField()
+    key_skills = models.TextField()
+    salary_from = models.FloatField()
+    salary_to = models.FloatField()
+    salary_currency = models.TextField()
+    area_name = models.TextField()
+    published_at = models.TextField()
 
     class Meta:
         db_table = 'vacancies'
